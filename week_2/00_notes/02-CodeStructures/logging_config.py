fa91b12a158a01ceba86a5420670df0447a39aa3
Hubert Ambales <Hubert.P.Ambales@gds.ey.com>
2025-09-09 13:34:02 +0800

    changes on the configuration

--- week_2/00_notes/02-CodeStructures/logging_config.py
+++ week_2/00_notes/02-CodeStructures/logging_config.py
@@ -2,7 +2,7 @@ import logging
 
 def setup_logging():
     logging.basicConfig(
-        filename= r'C:\Users\AC129VR\OneDrive - EY\Documents\GitHub\python-bootcamp-exercise\week_2\00_notes\01-LoggingSample\logs\app.log',
+        filename= r'C:\Users\AC129VR\OneDrive - EY\Documents\GitHub\python-bootcamp-exercise\week_2\00_notes\02-CodeStructures\app.log',
         level=logging.DEBUG,
         format='%(asctime)s - %(levelname)s - %(message)s'
     )
