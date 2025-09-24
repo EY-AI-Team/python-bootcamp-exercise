00e28eae54fc4f9bc7f6dc05c2d46ed68bb1af71
mhygz <migsdelossantos123@gmail.com>
2025-09-24 21:32:23 +0800

    modification

--- week_2/Migz/logging_config.py
+++ week_2/Migz/logging_config.py
@@ -1,1 +1,8 @@
-"test ni miggy"
+import logging
+
+def setup_logging():
+    logging.basicConfig(
+        filename= r'C:\Users\CS158EW\OneDrive - EY\Desktop\Python Training\python-bootcamp-exercise\week_2\Migz\logs.log',
+        level=logging.DEBUG,
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
